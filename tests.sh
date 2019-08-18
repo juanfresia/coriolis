f9e02def9d587f7f57993149d3e03903a5388662
AlejandroGarcia95 <alemgarcia95@gmail.com>
2019-08-18 15:54:46 -0300

    Changed command for running tests

--- tests.sh
+++ tests.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-ALL_TESTS=($(find tests -type f -name "*.py"))
-
-for TEST in "${ALL_TESTS[@]}"
-do
-  if [[ "$TEST" == *__init__.py ]]; then
-    continue
-  fi
-  TEST=${TEST//.py/}
-  TEST=${TEST//\//.}
-  echo
-  echo Running tests on file: $TEST
-  python3 -m unittest $TEST
-done
+python -m unittest discover -s tests -p '*.py' -t .
