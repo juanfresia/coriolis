2edb9093412b653b8c8c125f1ab12ca4ae6a4709
Juan Manuel Fresia <jfresia@medallia.com>
2019-10-01 00:30:25 -0300

    Added comments to rule validation
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- parser/JarlRuleValidator.py
+++ parser/JarlRuleValidator.py
@@ -1,47 +1,82 @@
 from .JarlParserExceptions import *
 
 def validate_filter_arguments(filter, scope_filter=None):
-    seen = set()
+    """Validates filter arguments (iterators and wildcards) alone.
+    Rules to apply:
+    - Each argument must have an unique identifier. The same identifier
+    cannot be used twice.
+
+    If validating a filter expression of the rule fact, the filter expression
+    of the rule scope is needed for the validation.
+    """
+
     args = filter.wildcards + filter.iterators
+    # Include filter arguments from scope, if any
     if scope_filter:
         args = args + scope_filter.wildcards + scope_filter.iterators
 
+    seen = set()
     for arg in args:
         if arg in seen:
             raise JarlArgumentAlreadyDeclared(arg)
         seen.add(arg)
 
 def validate_condition_arguments(filter, scope_filter=None):
+    """Validates filter conditions alone.
+    Rules to apply:
+    - Elements in a filter condition need previous declaration (in
+    filter arguments or previous scope filter arguments)
+    - Condition arguments must be of the same type (e.g. iterator vs iterator).
+    - Left side of the condition must have been declared in same filter clause.
+
+    If validating a filter expression of the rule fact, the filter expression
+    of the rule scope is needed for the validation.
+    """
+
     iterators = filter.iterators
     wildcards = filter.wildcards
     arguments = iterators + wildcards
 
+    # Previous scope filter arguments need to be separated
     scope_arguments = []
     if scope_filter:
         scope_arguments = scope_filter.iterators + scope_filter.wildcards
 
     for cond in filter.conditions:
+        # Left side should always be declared in current filter
         if not cond.l in arguments:
             raise JarlArgumentNotDeclared(cond.l)
 
         # TODO: add checks in literal
+        # A literal condition needs no further checking (for now)
         if cond.is_literal:
             continue
 
+        # Right side CAN be declared in either the previous scope or the current filter
         if not cond.r in arguments and not cond.r in scope_arguments:
             raise JarlArgumentNotDeclared(cond.r)
 
+        # If right side is not in the previous scope, arguments must be of the same type
         if cond.l in iterators and cond.r in wildcards:
             raise JarlConditionMixesArguments(cond.l, cond.r)
         if cond.l in wildcards and cond.r in iterators:
             raise JarlConditionMixesArguments(cond.l, cond.r)
 
 def validate_selector(selector, filter=None):
+    """Validates selector alone.
+    Rules to apply:
+    - Arguments in all checkpoints must have been previously declared.
+
+    The scope filter, if any, is needed to completely validate a selector.
+    """
+
+    # Get known arguments for checkpoints
     if not filter:
         filter_args = []
     else:
         filter_args = filter.arguments()
 
+    # Make sure every argument is defined
     for chk in selector.get_checkpoints():
         for arg in chk.arguments:
             if not arg in filter_args:
@@ -50,6 +85,8 @@ def validate_selector(selector, filter=None):
 
 def validate_rule(rule):
     scope_filter = None
+
+    # Validate scope, if any
     if rule.scope:
         if rule.scope.filter:
             scope_filter = rule.scope.filter
@@ -57,6 +94,7 @@ def validate_rule(rule):
             validate_condition_arguments(scope_filter)
         validate_selector(rule.scope.selector, scope_filter)
 
+    # Validate fact
     if rule.fact.filter:
         validate_filter_arguments(rule.fact.filter, scope_filter=scope_filter)
         validate_condition_arguments(rule.fact.filter, scope_filter=scope_filter)
