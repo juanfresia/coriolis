8c776df8d7cfc53013427c4298e1f83e299c349d
Juan Manuel Fresia <jfresia@medallia.com>
2019-10-03 23:09:20 -0300

    Added rule for basic scope adapter
    Signed-off-by: Juan Manuel Fresia <jfresia@medallia.com>

--- tests/parser/jarl_adapter.py
+++ tests/parser/jarl_adapter.py
@@ -8,7 +8,7 @@ from common.aggregation_steps import *
 from common.jarl_rule import *
 
 class TestAdapter(unittest.TestCase):
-    def test_basic_adapter(self):
+    def test_adapter_with_basic_fact(self):
         rule = """
         rule readers_read_two_times
         for every r and any room, msg
@@ -30,7 +30,43 @@ class TestAdapter(unittest.TestCase):
 
         self.assertEqual(expected_rule_adapted, steps)
 
-    def test_basic_adapter_with_reference_to_scope(self):
+    def test_adapter_with_basic_scope(self):
+
+        rule = """
+        rule two_hydrogen_per_molecule
+        between water_made() and next water_made()
+        for every atom_type and any atom_id with atom_type='H'
+        bond(atom_type, atom_id) must happen 2 times
+        """
+
+        rules = parse_str(rule)
+        steps = JarlRuleAdapter().rule_to_steps(rules[0])
+
+        rule_header = "two_hydrogen_per_molecule"
+        rule_scope = RuleScope([
+            MatchCheckpoints(["water_made"]),
+            CrossAndGroupByArgs([["water_made"], ["water_made"]]),
+            ScopeBetween("water_made", "water_made")
+        ])
+        rule_fact = RuleFact([
+            MatchCheckpoints(["bond"]),
+            RenameArgs([["bond", "atom_type", "atom_id"]]),
+            CrossAndGroupByArgs([["bond", "atom_type"]]),
+            ImposeIteratorCondition("atom_type", "=", "H", True),
+            CompareResultsQuantity("=", 2),
+            ReduceResult()
+        ])
+
+        # TODO stuff: remove me when the rule is fully parsed
+        steps.scope = rule_scope
+        steps.passed_by_default = True
+
+        expected_rule_adapted = JARLRule(rule, rule_header, rule_fact, rule_scope)
+        self.assertEqual(expected_rule_adapted, steps)
+
+        pass
+
+    def test_adapter_with_reference_to_scope(self):
         rule = """
         rule reader_reads_what_was_last_written
         for every r1, m1, r2, m2 and any r, w with m1!='NULL', r2=r1, m2=m1
@@ -66,13 +102,14 @@ class TestAdapter(unittest.TestCase):
         expected_rule_adapted.set_dynamic_scope_arg("r2", True)
         expected_rule_adapted.set_dynamic_scope_arg("m2", True)
 
-        # TODO stuff
+        # TODO stuff: remove me when the rule is fully parsed
         steps.scope = rule_scope
         steps.passed_by_default = True
         steps.set_dynamic_scope_arg("r2", True)
         steps.set_dynamic_scope_arg("m2", True)
 
         self.assertEqual(expected_rule_adapted, steps)
+
 
 if __name__ == '__main__':
     unittest.main()
