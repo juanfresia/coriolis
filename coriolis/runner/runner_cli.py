f6fe125b5e5a2f80f5f0de3de3aade40b43160c7
alejandrogarcia <alemgarcia95@gmail.com>
2019-10-26 16:18:26 -0300

    First dockerized Runner WIP

--- coriolis/runner/runner_cli.py
+++ coriolis/runner/runner_cli.py
@@ -2,9 +2,12 @@
 
 import os
 import distutils.dir_util
-
+import docker  # pip3 install docker
+from uuid import uuid4
 from runner import lang_instrumenter
 from runner.runner_printer import RunnerPrinter
+
+TMP_DIR = "/tmp/coriolis"
 
 
 class Runner:
@@ -13,8 +16,23 @@ class Runner:
         self.printer = RunnerPrinter(verbose_mode)
         self.language = language
         self.checkpoints = checkpoints
+        self.docker_client = docker.from_env()
+        self.id = str(uuid4())[-12:]
 
-    def instrument(self, src_dir, dst_dir):
+    def _get_image(self):
+        # TODO: Build images as "coriolis_{self.language}" ?
+        return {
+            "c": "coriolis_cpp",
+            "cpp": "coriolis_cpp",
+            "py": "coriolis_python",
+            "rs": "coriolis_rust"
+        }[self.language]
+
+    def _get_tmp_dir(self):
+        return "{}/{}/".format(TMP_DIR, self.id)
+
+    def instrument(self, src_dir):
+        dst_dir = self._get_tmp_dir()
         self.printer.print_instrument_summary(src_dir, dst_dir, self.language, self.checkpoints)
         try:
             distutils.dir_util.copy_tree(src_dir, dst_dir)
@@ -29,11 +47,26 @@ class Runner:
 
                 if self.instrumenter.can_instrument(file):
                     self.instrumenter.instrument_file_inline(file)
-                    self.printer.print_instrument_file(file, True)
+                    self.printer.print_instrument_file(file[len(dst_dir):], True)
                 else:
-                    self.printer.print_instrument_file(file, False)
+                    self.printer.print_instrument_file(file[len(dst_dir):], False)
+
+    def run_code(self, logs_dir):
+        image = self._get_image()
+        src_dir = self._get_tmp_dir()
+        volumes = {src_dir: {"bind": "/src", "mode": "rw"}, logs_dir: {"bind": "/logs", "mode": "rw"}}
+        docker_args = {
+            "remove": True,
+            "stdin_open": True,
+            "tty": True,
+            "init": True,
+            "detach": True,
+            "volumes": volumes
+        }
+        self.docker_client.containers.run(image, **docker_args)
 
 
-def run_instrumenter(args):
+def run_runner(args):
     coriolis_runner = Runner(args.language[0], args.checkpoints, args.verbose)
-    coriolis_runner.instrument(args.source, args.destination)
+    coriolis_runner.instrument(args.source)
+    coriolis_runner.run_code(args.destination)
