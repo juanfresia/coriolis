8bd441c594c45d40ff4f149d9d8891ed2d8764e2
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2019-10-27 17:37:13 -0300

    Use flag to install systemd unit
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- coriolis/docker/generate-package.sh
+++ coriolis/docker/generate-package.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 mkdir -p pkg/usr/local/sbin
-mkdir -p pkg/etc/systemd/system
-
 cp dist/coriolis pkg/usr/local/sbin
-cp docker/coriolis-mongo.service pkg/etc/systemd/system
 
-fpm -s dir -t deb -v ${VERSION} -n coriolis -C pkg -p dist .
+fpm -s dir -t deb --deb-systemd docker/coriolis-mongo.service -v ${VERSION} -n coriolis -C pkg -p dist .
